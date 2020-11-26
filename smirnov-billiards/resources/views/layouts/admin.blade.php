7446c01d2830595e86b1a190e0bc3dd1d2f5b577
Ярослав Лук'янчук <yarikthe@gmail.com>
2020-11-26 11:11:49 +0200

    w

--- smirnov-billiards/resources/views/layouts/admin.blade.php
+++ smirnov-billiards/resources/views/layouts/admin.blade.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Label - Premium Responsive Bootstrap 4 Admin & Dashboard Template</title>
+    <title>Smirnov - Billiards</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="../assets/vendors/iconfonts/mdi/css/materialdesignicons.css">
+    <link rel="stylesheet" href="/assets/vendors/iconfonts/mdi/css/materialdesignicons.css">
     <!-- endinject -->
     <!-- vendor css for this page -->
     <!-- End vendor css for this page -->
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="/assets/css/demo_1/style.css">
     <!-- Layout style -->
     <link rel="shortcut icon" href="https://img.icons8.com/cotton/452/billiard-ball.png" />
+
   </head>
   <body class="header-fixed">
     <!-- partial:partials/_header.html -->
@@ -31,136 +32,8 @@
           <button class="t-header-toggler t-header-mobile-toggler d-block d-lg-none">
             <i class="mdi mdi-menu"></i>
           </button>
-          <form action="#" class="t-header-search-box">
-            <div class="input-group">
-              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Search" autocomplete="off">
-              <button class="btn btn-primary" type="submit"><i class="mdi mdi-arrow-right-thick"></i></button>
-            </div>
-          </form>
           <ul class="nav ml-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="#" id="notificationDropdown" data-toggle="dropdown" aria-expanded="false">
-                <i class="mdi mdi-bell-outline mdi-1x"></i>
-              </a>
-              <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="notificationDropdown">
-                <div class="dropdown-header">
-                  <h6 class="dropdown-title">Notifications</h6>
-                  <p class="dropdown-title-text">You have 4 unread notification</p>
-                </div>
-                <div class="dropdown-body">
-                  <div class="dropdown-list">
-                    <div class="icon-wrapper rounded-circle bg-inverse-primary text-primary">
-                      <i class="mdi mdi-alert"></i>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Storage Full</small>
-                      <small class="content-text">Server storage almost full</small>
-                    </div>
-                  </div>
-                  <div class="dropdown-list">
-                    <div class="icon-wrapper rounded-circle bg-inverse-success text-success">
-                      <i class="mdi mdi-cloud-upload"></i>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Upload Completed</small>
-                      <small class="content-text">3 Files uploded successfully</small>
-                    </div>
-                  </div>
-                  <div class="dropdown-list">
-                    <div class="icon-wrapper rounded-circle bg-inverse-warning text-warning">
-                      <i class="mdi mdi-security"></i>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Authentication Required</small>
-                      <small class="content-text">Please verify your password to continue using cloud services</small>
-                    </div>
-                  </div>
-                </div>
-                <div class="dropdown-footer">
-                  <a href="#">View All</a>
-                </div>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="#" id="messageDropdown" data-toggle="dropdown" aria-expanded="false">
-                <i class="mdi mdi-message-outline mdi-1x"></i>
-                <span class="notification-indicator notification-indicator-primary notification-indicator-ripple"></span>
-              </a>
-              <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="messageDropdown">
-                <div class="dropdown-header">
-                  <h6 class="dropdown-title">Messages</h6>
-                  <p class="dropdown-title-text">You have 4 unread messages</p>
-                </div>
-                <div class="dropdown-body">
-                  <div class="dropdown-list">
-                    <div class="image-wrapper">
-                      <img class="profile-img" src="../assets/images/profile/male/image_1.png" alt="profile image">
-                      <div class="status-indicator rounded-indicator bg-success"></div>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Clifford Gordon</small>
-                      <small class="content-text">Lorem ipsum dolor sit amet.</small>
-                    </div>
-                  </div>
-                  <div class="dropdown-list">
-                    <div class="image-wrapper">
-                      <img class="profile-img" src="../assets/images/profile/female/image_2.png" alt="profile image">
-                      <div class="status-indicator rounded-indicator bg-success"></div>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Rachel Doyle</small>
-                      <small class="content-text">Lorem ipsum dolor sit amet.</small>
-                    </div>
-                  </div>
-                  <div class="dropdown-list">
-                    <div class="image-wrapper">
-                      <img class="profile-img" src="../assets/images/profile/male/image_3.png" alt="profile image">
-                      <div class="status-indicator rounded-indicator bg-warning"></div>
-                    </div>
-                    <div class="content-wrapper">
-                      <small class="name">Lewis Guzman</small>
-                      <small class="content-text">Lorem ipsum dolor sit amet.</small>
-                    </div>
-                  </div>
-                </div>
-                <div class="dropdown-footer">
-                  <a href="#">View All</a>
-                </div>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="#" id="appsDropdown" data-toggle="dropdown" aria-expanded="false">
-                <i class="mdi mdi-apps mdi-1x"></i>
-              </a>
-              <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="appsDropdown">
-                <div class="dropdown-header">
-                  <h6 class="dropdown-title">Apps</h6>
-                  <p class="dropdown-title-text mt-2">Authentication required for 3 apps</p>
-                </div>
-                <div class="dropdown-body border-top pt-0">
-                  <a class="dropdown-grid">
-                    <i class="grid-icon mdi mdi-jira mdi-2x"></i>
-                    <span class="grid-tittle">Jira</span>
-                  </a>
-                  <a class="dropdown-grid">
-                    <i class="grid-icon mdi mdi-trello mdi-2x"></i>
-                    <span class="grid-tittle">Trello</span>
-                  </a>
-                  <a class="dropdown-grid">
-                    <i class="grid-icon mdi mdi-artstation mdi-2x"></i>
-                    <span class="grid-tittle">Artstation</span>
-                  </a>
-                  <a class="dropdown-grid">
-                    <i class="grid-icon mdi mdi-bitbucket mdi-2x"></i>
-                    <span class="grid-tittle">Bitbucket</span>
-                  </a>
-                </div>
-                <div class="dropdown-footer">
-                  <a href="#">View All</a>
-                </div>
-              </div>
-            </li>
-            <li>
+
             <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
@@ -175,14 +48,14 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                 ADMIN   {{ Auth::user()->name }}
+                                <i>Адміністратор #{{ Auth::user()->id }}</i>    {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Вийти') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -205,96 +78,51 @@
             <img class="profile-img img-lg rounded-circle" src="../assets/images/profile/male/image_1.png" alt="profile image">
           </div>
           <div class="info-wrapper">
+            <i>Адміністратор</i>
             <p class="user-name">{{ Auth::user()->name }}</p>
-            <h6 class="display-income">${{ Auth::user()->balance }}</h6>
           </div>
         </div>
         <ul class="navigation-menu">
-          <li class="nav-category-divider">MAIN</li>
+          <li class="nav-category-divider">Меню</li>
           <li>
-            <a href="index.html">
-              <span class="link-title">Dashboard</span>
-              <i class="mdi mdi-gauge link-icon"></i>
+            <a href="/home">
+              <span class="link-title">Головна</span>
             </a>
           </li>
           <li>
-            <a href="#sample-pages" data-toggle="collapse" aria-expanded="false">
-              <span class="link-title">Sample Pages</span>
-              <i class="mdi mdi-flask link-icon"></i>
+            <a href="/home">
+              <span class="link-title">Користувачі</span>
             </a>
-            <ul class="collapse navigation-submenu" id="sample-pages">
-              <li>
-                <a href="pages/sample-pages/login_1.html" target="_blank">Login</a>
-              </li>
-              <li>
-                <a href="pages/sample-pages/error_2.html" target="_blank">Error</a>
-              </li>
-            </ul>
           </li>
           <li>
-            <a href="#ui-elements" data-toggle="collapse" aria-expanded="false">
-              <span class="link-title">UI Elements</span>
-              <i class="mdi mdi-bullseye link-icon"></i>
+            <a href="/home">
+              <span class="link-title">Гравці</span>
             </a>
-            <ul class="collapse navigation-submenu" id="ui-elements">
-              <li>
-                <a href="pages/ui-components/buttons.html">Buttons</a>
-              </li>
-              <li>
-                <a href="pages/ui-components/tables.html">Tables</a>
-              </li>
-              <li>
-                <a href="pages/ui-components/typography.html">Typography</a>
-              </li>
-            </ul>
           </li>
+          <li>
+            <a href="/home">
+              <span class="link-title">Турніри</span>
+            </a>
+          </li>
+          
           <li>
             <a href="pages/forms/form-elements.html">
-              <span class="link-title">Forms</span>
-              <i class="mdi mdi-clipboard-outline link-icon"></i>
-            </a>
-          </li>
-          <li>
-            <a href="pages/charts/chartjs.html">
-              <span class="link-title">Charts</span>
-              <i class="mdi mdi-chart-donut link-icon"></i>
+              <span class="link-title">Статистика</span>
             </a>
           </li>
           <li>
             <a href="pages/icons/material-icons.html">
-              <span class="link-title">Icons</span>
-              <i class="mdi mdi-flower-tulip-outline link-icon"></i>
+              <span class="link-title">Прогноз</span>
             </a>
           </li>
-          <li class="nav-category-divider">DOCS</li>
-          <li>
-            <a href="../docs/docs.html">
-              <span class="link-title">Documentation</span>
-              <i class="mdi mdi-asterisk link-icon"></i>
-            </a>
-          </li>
+         
         </ul>
 
       </div>
       <!-- partial -->
       <div class="page-content-wrapper">
     @yield('content')
-     <!-- partial:partials/_footer.html -->
-     <footer class="footer">
-          <div class="row">
-            <div class="col-sm-6 text-center text-sm-right order-sm-1">
-              <ul class="text-gray">
-                <li><a href="#">Terms of use</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-              </ul>
-            </div>
-            <div class="col-sm-6 text-center text-sm-left mt-3 mt-sm-0">
-              <small class="text-muted d-block">Copyright © 2020 <a href="#" target="_blank">Smirnov - Biliard</a>. All rights reserved</small>
-              <small class="text-gray mt-2">Made With <i class="mdi mdi-heart text-danger"></i> by Lukyanchuk Media</small>
-            </div>
-          </div>
-        </footer>
-        <!-- partial -->
+
       </div>
       <!-- page content ends -->
     </div>
